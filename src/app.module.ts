b3586320ae88eef6be571f93a2a6d90691db054d
andela-aonifade <anuoluwapo.onifade@andela.com>
2023-11-07 22:12:48 -0700

    adds auth modules controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, ProductModule],
 })
 export class AppModule {}
